0869f46998811d7d5fa90482f5a38d3cb3c35d99
Nico E <earnshaw.nico@gmail.com>
2018-07-25 17:34:09 -0300

    multiple bird support

--- scene.tsx
+++ scene.tsx
@@ -6,84 +6,99 @@ export type BirdState = null | 'looking' | 'shake';
 
 // This is an interface, you can use it to enforce the types of your state
 export interface IState {
-  birdPos: Vector3Component,
-  birdState: BirdState,
+  birdPos: Vector3Component[],
+  birdState: BirdState[],
 }
 
 export default class garden extends DCL.ScriptableScene<any, IState> {
   // This is your initial state and it respects the given IState interface
   state = {
-    birdPos: {x:0, y:1, z:0},
-    birdState: null,
+    birdPos: [{x:0, y:1, z:0},{x:1, y:1, z:0}],
+    birdState: [null, null],
   }
 
   sceneDidMount() {
     this.eventSubscriber.on('door_click', () => {
   
     })
+   this.updateBird(0);
+   this.updateBird(1);
+  }
+
+  async updateBird(bird: number=0)
+  {
     setInterval(() => {
-      this.setState({birdPos: this.newBirdPos()});
-      this.setState({birdState: this.newBirdState()});
+      this.setState({birdPos: this.newBirdPos(bird)});
+      this.setState({birdState: this.newBirdState(bird)});
     }, 4000);
   }
 
-  newBirdPos()
+
+  newBirdPos(bird: number=0)
   {
-    const xPos = (Math.random() *10 )- 5;
-    const zPos = (Math.random() *10 )- 5;  
-    const yPos = Math.random() *2 + 1;
-    return {x:xPos , y:yPos, z:zPos};
+    let newPos : Vector3Component[] = this.state.birdPos;
+    newPos[bird].x = (Math.random() *10 )- 5;
+    newPos[bird].z = (Math.random() *10 )- 5;  
+    newPos[bird].y = Math.random() *2 + 1;
+    
+    return newPos;
   }
 
-  newBirdState()
+  newBirdState(bird: number=0)
   {
+    let newState : BirdState[] = this.state.birdState;
     const stateNow = Math.random();
     console.log(stateNow);
     if (stateNow < 0.6){ 
-      return null 
+      newState[bird] = null 
     } else if (stateNow < 0.8) {
-      return 'looking'
+      newState[bird] =  'looking'
     } else {
-      return 'shake'
+      newState[bird] =  'shake'
     }
+    return newState;
   }
+
+  renderBirds()
+  {
+    return this.state.birdPos.map( (pos, birdNum) =>  
+    <gltf-model
+      src="models/hummingbird.gltf"
+      scale={0.2}
+      position={this.state.birdPos[birdNum]}
+      lookAt={this.state.birdPos[birdNum]}
+      transition={{ 
+        position: { duration: 500, timing: "ease-in-out" }, 
+        //lookAt: { duration: 100, timing: "linear" } 
+      }}
+      skeletalAnimation={
+        [
+            { clip: "Armature_fly" , loop:true, playing:true },
+            { clip: "Armature_look" , playing:
+            this.state.birdState[birdNum] == 'looking'?true:false },
+            { clip: "Armature_shake" , playing: this.state.birdState[birdNum] == 'shake'}
+          ] 
+          
+      }
+    />
+
+  )
+  }
+
+
 
   async render() {
 
 
     return (
       <scene position={{ x: 5, y: 0, z: 5 }}>
-        <gltf-model
-          src="models/hummingbird.gltf"
-          scale={0.2}
-          position={this.state.birdPos}
-          lookAt={this.state.birdPos}
-          transition={{ 
-            position: { duration: 500, timing: "ease-in-out" }, 
-            //lookAt: { duration: 100, timing: "linear" } 
-        }}
-          skeletalAnimation={
-            this.state.birdState == 'looking'
-            ?[
-                { clip: "Armature_fly" , playing: true, loop:true },
-                { clip: "Armature_look" , playing: true, loop:true },
-                { clip: "Armature_shake" , playing: false, loop:false }
-              ]
-              :this.state.birdState == 'shake'
-              ?[
-                { clip: "Armature_fly" , playing: true, loop:true },
-                { clip: "Armature_look" , playing: false },
-                { clip: "Armature_shake" , playing: true, loop:true}
-              ]
-             :[
-                { clip: "Armature_fly" , playing: true, loop:true },
-                { clip: "Armature_look" , playing: false },
-                { clip: "Armature_shake" , playing: false}
-              ]
-          }
-         />
+        {this.renderBirds()}
 
-     
+        <gltf-model
+          src="models/apple tree.gltf"
+          scale={0.25}
+          id="tree"
+         />
       </scene>
     )
   }
